a73c625414f80fb64cf209cf2533409219dde28c
Sumeyse <sumeyse@gmail.com>
2024-02-28 17:59:07 +0300

    2 Questions added

--- DnaToRna.java
+++ DnaToRna.java
@@ -1,4 +1,9 @@
 public class DnaToRna {
 
-
+    public static void main(String[] args) {
+        System.out.println(dnaToRna("TTCCTTCC"));
+    }
+    public static String dnaToRna(String dna) {
+        return dna.replaceAll("T", "U");
+    }
 }
---
+++ SumOfDigits.java
@@ -0,0 +1,24 @@
+public class SumOfDigits {
+
+    /*Question-1 Sum of Digits
+Given a number, write a method that finds the sum of its digits.
+
+Examples :
+Input: n = 687
+Output: 21
+
+Input: n = 12
+Output: 3
+
+
+Question-2 Sum of Digits Until Producing Single Digit
+Digital root is the recursive sum of all the digits in a number.
+Given n, write a method that returns the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+*/
+}
